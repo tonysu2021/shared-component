cb46a7a6564236846fe5c712054ebfcd1344f126
Tony.su <tony.su@rhf-tech.com>
2021-09-01 11:08:16 +0800

    Adjust

--- commerce-cache/src/main/java/com/commerce/cache/client/TransactionHelper.java
+++ commerce-cache/src/main/java/com/commerce/cache/client/TransactionHelper.java
@@ -8,24 +8,31 @@ import org.springframework.integration.redis.util.RedisLockRegistry;
 import reactor.core.publisher.Mono;
 
 public class TransactionHelper {
-	
-    private RedisLockRegistry redisLockRegistry;
 
-    public TransactionHelper(RedisLockRegistry redisLockRegistry) {
+	private RedisLockRegistry redisLockRegistry;
+
+	public TransactionHelper(RedisLockRegistry redisLockRegistry) {
 		this.redisLockRegistry = redisLockRegistry;
 	}
 
 	/**
-     * Do supplier in a transaction protected by a distributed lock, lock key is given by param key.
-     * 
-     */
-    public <T> Mono<T> doInTransaction(String transactionKey, Supplier<Mono<T>> supplier) {
-        Lock lock = redisLockRegistry.obtain(transactionKey);
-        return Mono.just(0)
-                .doFirst(lock::lock)
-                .doFinally(dummy ->  lock.unlock())
-                .flatMap(dummy -> supplier.get())
+	 * Do supplier in a transaction protected by a distributed lock, lock key is
+	 * given by param key.
+	 * 
+	 * @param transactionKey 交易鎖的key name
+	 * @param supplier       緩存邏輯
+	 * 
+	 * @return
+	 * 
+	 */
+	public <T> Mono<T> doInTransaction(String transactionKey, Supplier<Mono<T>> supplier) {
+		Lock lock = redisLockRegistry.obtain(transactionKey);
+		return Mono.just(0)
+				.doFirst(lock::lock)
+				.doFinally(dummy -> lock.unlock())
+				.doOnError(Exception.class, event -> lock.unlock())
+				.flatMap(dummy -> supplier.get())
 //                .subscribeOn(Schedulers.newSingle(transactionKey))
-                .log("[Transaction]");
-    }
+				.log("[Transaction]");
+	}
 }
